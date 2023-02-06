9723c8fded15c11fab0dcbeda1f321c224aeef0c
WLHQ <106452668+WLHQ@users.noreply.github.com>
2023-02-06 17:49:13 -0500

    Delete 2D Cat Run Platfromer directory

--- 2D Cat Run Platfromer/2D Cat Run Platfromer Assets/Scripts/Nav-Button.gd
+++
@@ -1,4 +0,0 @@
-extends MenuButton
-# When pressed in game by the player, the scene will change back to the menu.
-func _on_MenuButton_pressed():
-	get_tree().change_scene("res://Menu.tscn")
--- 2D Cat Run Platfromer/2D Cat Run Platfromer Assets/Scripts/ScoreLabel.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-var score = 0
-# Updates the UI in the current level updating the label to reflect the players current score. 
-func _on_FishTreat_body_entered(body):
-	score += 1
-	text = "Score: %s" % score
